6f512c2ba9621f6885c04c2af8c56440f3b32f2c
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-07-06 11:27:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-string thisValue = "";
-decimal thisResult = 0;
-string thisOperation = "";
+List<Operations> calculations = new List<Operations>
+{
+    new() { ThValue = "", ThResult = 0, ThOperation = ""}
+};
 
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
@@ -8,4 +9,32 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapGet("/api/calculations", () => calculations);
+
+app.MapGet("/api/calculations", () =>
+{
+
+    Operations? calculation = calculations.FirstOrDefault();
+
+    return Results.Json(calculation);
+
+});
+
+app.MapPut("/api/calculations", (Operations calculationData) =>
+{
+
+    var calculation = calculations.FirstOrDefault();
+
+    calculation.ThValue = calculation.ThValue + calculationData.ThValue;
+
+    return Results.Json(calculation);
+
+});
+
 app.Run();
+public class Operations
+{
+    public string ThValue { get; set; } = "";
+    public decimal ThResult { get; set; } = 0;
+    public string ThOperation { get; set; } = "";
+}
